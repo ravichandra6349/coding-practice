7fabdd499b5fc3766408cac2b10c1f0720563374
Ravi chandra <121404669+ravichandra6349@users.noreply.github.com>
2022-12-26 02:34:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+/*console.log(typeof(MyName));
+var myage=26;
+console.log(typeof(myage));
+var im=true;
+console.log(typeof(im));
+-----------------------------------------------------------------------------*/
+
+var num=8982209310;
+var name="bittu";
+console.log(isNaN(num));
+console.log(isNaN(name));
---
+++ practice.js
@@ -0,0 +1,21 @@
+/*console.log(10+"20");
+console.log(9-"5");
+console.log("java "+"script");
+console.log("bittu"-"yadav");
+console.log(true+true);
+
+
+var a=null;
+console.log(typeof(a));
+
+
+var b=6;
+console.log(b);
+console.log(typeof(b));*/
+
+
+console.log(NaN===NaN);
+console.log(isNaN(NaN));
+console.log(Number.NaN===NaN);
+console.log(isNaN(Number.NaN));
+console.log(Number.isNaN(NaN));
